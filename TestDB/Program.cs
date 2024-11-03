328fcaa5bf68b85a67f8007df6e0b40ffddf0b3e
QuocAnh-dev <quocanh.dev404@gmail.com>
2024-11-03 11:02:35 +0700

    database test project

---
+++ TestDB/Program.cs
@@ -0,0 +1,17 @@
+﻿using BusinessObjects;
+using DataAccessLayer;
+
+namespace TestDB
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using var context = new FinanceManagementApplicationContext();
+            List<User> users = context.Users.ToList();
+            foreach (var user in users) { 
+                Console.WriteLine(user.Username);
+            }
+        }
+    }
+}
