210c38040321244a8df3d37022ae4f5a7e4bb9eb
Hoang Vu Hung <hoanghung250604@gmail.com>
2024-11-14 13:42:27 +0700

    view all budget item at ui

---
+++ BusinessObjects/Util.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessObjects
+{
+    public class Util
+    {
+        private static Util _instance;
+        public static Util Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = new Util();
+                }
+                return _instance;
+            }
+        }
+
+        private Util() { }
+
+        public string FormatMoney(int money)
+        {
+            return money.ToString("N0", System.Globalization.CultureInfo.GetCultureInfo("vi-VN")) + ".000";
+        }
+    }
+}
