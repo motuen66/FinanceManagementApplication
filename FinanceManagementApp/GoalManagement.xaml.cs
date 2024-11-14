3c0e6c069f87a6262331d91710b469eaf9237987
minhthang <minhthangqhqn@gmail.com>
2024-11-14 12:02:05 +0700

    done-goal-window

--- FinanceManagementApp/GoalManagement.xaml.cs
+++ FinanceManagementApp/GoalManagement.xaml.cs
@@ -1,4 +1,6 @@
 ﻿using FinanceManagementApp.Domain;
+using LiveCharts;
+using LiveCharts.Wpf;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -21,10 +23,20 @@ namespace FinanceManagementApp
     /// </summary>
     public partial class GoalManagement : UserControl
     {
+        public List<string> MonthLabels { get; set; }
+        public SeriesCollection ExpenseSeries { get; set; }
         public GoalManagement()
         {
             InitializeComponent();
             userHeaderControl.ChangedTitleAndSubTitle(ScreenType.Goal);
+            ExpenseSeries = new SeriesCollection
+        {
+            new ColumnSeries
+            {
+                Values = new ChartValues<double> { 1200.5, 1500.3, 1800.4, 1000.2, 2000.1, 2200.7, 1700.5, 1900.3, 1600.0, 1800.8, 1400.9, 2100.0 }
+            }
+        };
+            DataContext = this;
         }
     }
 }
